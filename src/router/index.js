6261348a1956f65325a3b6cab897d4d79fd2ecb3
杨森博 <1041178770@qq.com>
2020-04-06 21:53:41 +0800

    obj

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import routes from './routes'
 
 
 //修正vue原型上的push和replace方法
-const originPush=VueRoyter.prototype.push
+const originPush=VueRouter.prototype.push
 //给成功回调指定默认函数
 VueRouter.prototype.push=function(location,onComplete=()=>{},onAbort){
   return originPush.call(this,location,onComplete,onAbort)
@@ -19,10 +19,10 @@ VueRouter.prototype.replace=function(location,onComplete,onAbort){
   return originPush.call(this,location,onComplete,onAbort).catch(()=>{})
 }
 
-vue.use(VueRouter)
+Vue.use(VueRouter)
 //向外暴露路由器对象
 export default new VueRouter({
   //mode:'hash',路由路径带//#
-  //mode:'history',路由路径不带#
+  mode:'history',//路由路径不带#
   routes//配置应用中的所有路由
 })
